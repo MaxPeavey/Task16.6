787110a382496f2bc5c4e0b2262ed0c2b9e31a28
MaxPeavey <44377538+MaxPeavey@users.noreply.github.com>
2025-12-12 11:38:50 +0300

    Naming Update

--- Task16.6.Tests/UnitTest1.cs
+++ Task16.6.Tests/UnitTest1.cs
@@ -9,49 +9,49 @@ public class Tests
         private readonly Calculator _calculator = new();
         
         [Test]
-        public void Additional_ReturnsCorrectValue()
+        public void Additional_MustReturnsCorrectValue()
         {
            Assert.That(_calculator.Additional(7,4), Is.EqualTo(11)); 
         }
         
         [Test]
-        public void Additional_ReturnsCorrectType()
+        public void Additional_MustReturnsCorrectType()
         {
             Assert.That(_calculator.Additional(7, 4).GetType(), Is.EqualTo(typeof(int)));
         }
         
         [Test]
-        public void Subtraction_ReturnsCorrectValue()
+        public void Subtraction_MustReturnsCorrectValue()
         {
             Assert.That(_calculator.Subtraction(7, 4), Is.EqualTo(3));
         }
         
         [Test]
-        public void Subtraction_ReturnsCorrectType()
+        public void Subtraction_MustReturnsCorrectType()
         {
             Assert.That(_calculator.Subtraction(7, 4).GetType(), Is.EqualTo(typeof(int)));
         }
         
         [Test]
-        public void Multiplication_ReturnsCorrectValue()
+        public void Multiplication_MustReturnsCorrectValue()
         {
             Assert.That(_calculator.Miltiplication(7,7),  Is.EqualTo(49));
         }
         
         [Test]
-        public void Multiplication_ReturnsCorrectType()
+        public void Multiplication_MustReturnsCorrectType()
         {
             Assert.That(_calculator.Miltiplication(7,7),  Is.EqualTo(49));
         }
         
         [Test]
-        public void Division_ReturnsCorrectValue()
+        public void Division_MustReturnsCorrectValue()
         {
             Assert.That(_calculator.Division(14,7), Is.EqualTo(2));
         }
         
         [Test]
-        public void Division_ReturnsCorrectType()
+        public void Division_MustReturnsCorrectType()
         {
             Assert.That(_calculator.Division(14,7).GetType(), Is.EqualTo(typeof(int)));
         }
